3647d8d70d62d1b277edeebd2616d846f0102c54
Robert Moore <rob@owlplatform.com>
2014-07-28 17:17:32 -0400

    Switching to Atomic Integer.

--- src/main/java/com/owlplatform/aggregator/CachingFilteringSolverInterface.java
+++ src/main/java/com/owlplatform/aggregator/CachingFilteringSolverInterface.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,133 +35,131 @@ import com.owlplatform.solver.rules.SubscriptionRequestRule;
 
 public class CachingFilteringSolverInterface extends SolverInterface {
 
-  /**
-   * Logger for this class.
-   */
-  private static final Logger log = LoggerFactory
-      .getLogger(CachingFilteringSolverInterface.class);
+	/**
+	 * Logger for this class.
+	 */
+	private static final Logger log = LoggerFactory
+			.getLogger(CachingFilteringSolverInterface.class);
 
-  private static final int MAX_DEVICES = 200;
+	private static final int MAX_DEVICES = 200;
 
-  ConcurrentLinkedQueue<SubscriptionRequestRule> effectiveRules = new ConcurrentLinkedQueue<SubscriptionRequestRule>();
+	ConcurrentLinkedQueue<SubscriptionRequestRule> effectiveRules = new ConcurrentLinkedQueue<SubscriptionRequestRule>();
 
-  Map<HashableByteArray, DeviceIdHashEntry> ruleCache = Collections
-      .synchronizedMap(new LRUCache<HashableByteArray, DeviceIdHashEntry>(
-          MAX_DEVICES));
+	Map<HashableByteArray, DeviceIdHashEntry> ruleCache = Collections
+			.synchronizedMap(new LRUCache<HashableByteArray, DeviceIdHashEntry>(
+					MAX_DEVICES));
 
-  protected boolean hasEffectiveRules = false;
+	protected boolean hasEffectiveRules = false;
 
-  protected volatile boolean reportedDrop = false;
+	protected volatile boolean reportedDrop = false;
 
-  protected volatile int numDropped = 0;
+	protected volatile AtomicInteger numDropped = new AtomicInteger(0);
 
-  @Override
-  public synchronized boolean sendSample(SampleMessage sampleMessage) {
+	@Override
+	public synchronized boolean sendSample(SampleMessage sampleMessage) {
 
-    if (this.session.getScheduledWriteMessages() > SolverInterface.MAX_OUTSTANDING_SAMPLES) {
-      if (!this.reportedDrop) {
-        this.reportedDrop = true;
-        log.debug("Dropping a sample for {}", this);
-      }
-      ++this.numDropped;
-      return false;
-    }
-    if (this.reportedDrop) {
-      log.warn("{} lost {} samples.", this, this.numDropped);
-      this.numDropped = 0;
-      this.reportedDrop = false;
-    }
+		if (this.session.getScheduledWriteMessages() > SolverInterface.MAX_OUTSTANDING_SAMPLES) {
+			this.numDropped.incrementAndGet();
+			
+			return false;
+		}
 
-    if (!this.sentSubscriptionResponse) {
-      return false;
-    }
+		if (!this.sentSubscriptionResponse) {
+			return false;
+		}
 
-    if (!this.hasEffectiveRules) {
-      return super.sendSample(sampleMessage);
-    }
+		if (!this.hasEffectiveRules) {
+			return super.sendSample(sampleMessage);
+		}
 
-    HashableByteArray deviceHasher = new HashableByteArray(
-        sampleMessage.getDeviceId());
+		HashableByteArray deviceHasher = new HashableByteArray(
+				sampleMessage.getDeviceId());
 
-    DeviceIdHashEntry cacheResult = this.ruleCache.get(deviceHasher);
+		DeviceIdHashEntry cacheResult = this.ruleCache.get(deviceHasher);
 
-    if (cacheResult == null) {
-      SubscriptionRequestRule passedRule = null;
-      for (SubscriptionRequestRule rule : this.effectiveRules) {
-        log.debug("Checking rule {}.", rule);
-        if (SubscriptionRuleFilter.applyRule(rule, sampleMessage)) {
-          passedRule = rule;
-          break;
-        }
-      }
+		if (cacheResult == null) {
+			SubscriptionRequestRule passedRule = null;
+			for (SubscriptionRequestRule rule : this.effectiveRules) {
+				log.debug("Checking rule {}.", rule);
+				if (SubscriptionRuleFilter.applyRule(rule, sampleMessage)) {
+					passedRule = rule;
+					break;
+				}
+			}
 
-      if (passedRule != null) {
-        log.debug("{} passed all rules.", sampleMessage);
-        DeviceIdHashEntry hashEntry = new DeviceIdHashEntry();
-        hashEntry.setPassedRules(true);
-        hashEntry.setUpdateInterval(passedRule.getUpdateInterval());
-        hashEntry.setNextPermittedTransmit(
-            sampleMessage.getReceiverId(),
-            System.currentTimeMillis()
-                + passedRule.getUpdateInterval());
-        this.ruleCache.put(deviceHasher, hashEntry);
-        return super.sendSample(sampleMessage);
-      }
-      DeviceIdHashEntry hashEntry = new DeviceIdHashEntry();
-      hashEntry.setPassedRules(false);
-      this.ruleCache.put(deviceHasher, hashEntry);
-      return false;
-    }
-    if (cacheResult.isPassedRules()) {
-      long now = System.currentTimeMillis();
-      long nextTransmit = cacheResult
-          .getNextPermittedTransmit(sampleMessage.getReceiverId());
-      if (nextTransmit <= now) {
-        cacheResult.setNextPermittedTransmit(
-            sampleMessage.getReceiverId(),
-            now + cacheResult.getUpdateInterval());
-        return super.sendSample(sampleMessage);
-      }
-      return false;
-    }
+			if (passedRule != null) {
+				log.debug("{} passed all rules.", sampleMessage);
+				DeviceIdHashEntry hashEntry = new DeviceIdHashEntry();
+				hashEntry.setPassedRules(true);
+				hashEntry.setUpdateInterval(passedRule.getUpdateInterval());
+				hashEntry.setNextPermittedTransmit(
+						sampleMessage.getReceiverId(),
+						System.currentTimeMillis()
+								+ passedRule.getUpdateInterval());
+				this.ruleCache.put(deviceHasher, hashEntry);
+				return super.sendSample(sampleMessage);
+			}
+			DeviceIdHashEntry hashEntry = new DeviceIdHashEntry();
+			hashEntry.setPassedRules(false);
+			this.ruleCache.put(deviceHasher, hashEntry);
+			return false;
+		}
+		if (cacheResult.isPassedRules()) {
+			long now = System.currentTimeMillis();
+			long nextTransmit = cacheResult
+					.getNextPermittedTransmit(sampleMessage.getReceiverId());
+			if (nextTransmit <= now) {
+				cacheResult.setNextPermittedTransmit(
+						sampleMessage.getReceiverId(),
+						now + cacheResult.getUpdateInterval());
+				return super.sendSample(sampleMessage);
+			}
+			return false;
+		}
 
-    return false;
-  }
+		return false;
+	}
 
-  public Collection<SubscriptionRequestRule> getEffectiveRules() {
-    return this.effectiveRules;
-  }
+	public Collection<SubscriptionRequestRule> getEffectiveRules() {
+		return this.effectiveRules;
+	}
 
-  /**
-   * A convenience method for adding a single rule to this Solver.
-   * 
-   * @param newRule the rule to add to this Solver.
-   */
-  public void addEffectiveRule(SubscriptionRequestRule newRule) {
+	/**
+	 * A convenience method for adding a single rule to this Solver.
+	 * 
+	 * @param newRule
+	 *            the rule to add to this Solver.
+	 */
+	public void addEffectiveRule(SubscriptionRequestRule newRule) {
 
-    this.effectiveRules.add(newRule);
+		this.effectiveRules.add(newRule);
 
-    for (SubscriptionRequestRule rule : this.effectiveRules) {
-      if (rule.getNumTransmitters() > 0 || rule.getPhysicalLayer() != 0
-          || rule.getUpdateInterval() > 0) {
-        this.hasEffectiveRules = true;
-        break;
-      }
-    }
-    log.info("Added {} to {}.", newRule, this);
-  }
+		for (SubscriptionRequestRule rule : this.effectiveRules) {
+			if (rule.getNumTransmitters() > 0 || rule.getPhysicalLayer() != 0
+					|| rule.getUpdateInterval() > 0) {
+				this.hasEffectiveRules = true;
+				break;
+			}
+		}
+		log.info("Added {} to {}.", newRule, this);
+	}
 
-  public void clearEffectiveRules() {
-    this.effectiveRules.clear();
-    this.hasEffectiveRules = false;
-    log.debug("Cleared effective rules.");
-  }
+	public int getAndClearDroppedPackets() {
 
-  @Override
-  public String toString() {
-    StringBuffer sb = new StringBuffer();
-    sb.append("CachingFilteringSolver @").append(
-        this.session.getRemoteAddress());
-    return sb.toString();
-  }
+		return this.numDropped.getAndSet(0);
+	}
+
+	public void clearEffectiveRules() {
+		this.effectiveRules.clear();
+		this.hasEffectiveRules = false;
+		log.debug("Cleared effective rules.");
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("CachingFilteringSolver @").append(
+				this.session.getRemoteAddress());
+		return sb.toString();
+	}
 }
